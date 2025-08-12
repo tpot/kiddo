2c979519ceb3bdd7b313580fc4f8d2a9454dad81
Tim Potter <tim.potter@canberra.edu.au>
2025-08-12 14:27:15 +1000

    Implement command to log in with emoji code and get user info

---
+++ kiddo/api.py
@@ -0,0 +1,56 @@
+import requests
+from bs4 import BeautifulSoup
+from http.client import HTTPConnection
+
+class StudentLogin:
+
+    def __init__(self, base_url, debug=False):
+
+        # Enable debugging for this session (actually all HTTP connections
+        # but blah)
+        if debug:
+            HTTPConnection.debuglevel = 1
+
+        self.base_url = base_url
+        self.session = requests.Session()
+
+    def login(self, emoji_code):
+        """Log in using an emoji code."""
+
+        # Get base page
+        resp = self.session.get(f"{self.base_url}/emoji-pass/")
+        resp.raise_for_status()
+
+        # Pull out CSRF token
+        soup = BeautifulSoup(resp.text, "html.parser")
+
+        input_tag = soup.find("input", {"name": "csrfmiddlewaretoken"})
+        csrf_token = input_tag["value"]
+
+        # Do emoji login request
+        params = {
+            "headers": {
+                "Referer": f"{self.base_url}/emoji-pass/",
+            },
+
+            "cookies": {
+                "csrftoken": csrf_token,
+            },
+
+            "data": {
+                "csrfmiddlewaretoken": csrf_token,
+                "emoji_pass": emoji_code,
+            },
+        }
+
+        post_resp = self.session.post(
+            f"{self.base_url}/emoji-pass/", **params)
+        post_resp.raise_for_status()
+
+    def me(self):
+        """Return information about logged in user."""
+
+        resp = self.session.get(f"{self.base_url}/api/me")
+        resp.raise_for_status()
+
+        return resp.json()
--- kiddo/cli.py
+++ kiddo/cli.py
@@ -1,2 +1,47 @@
-def main():
-    print("Hello from kiddo!")
+import click
+from api import StudentLogin
+
+@click.group()
+@click.option("--verbose", "-v", is_flag=True)
+@click.option("--debug", is_flag=True)
+@click.option("--base-url", envvar=["KIDDO_BASE_URL"])
+@click.pass_context
+def cli(ctx, verbose, debug, base_url):
+
+    ctx.obj = {
+        "verbose": verbose,
+        "debug": debug,
+        "base_url": base_url,
+    }
+
+@cli.command()
+@click.pass_context
+@click.option("--emoji-code", required=True)
+def emoji_login(ctx, emoji_code):
+
+    verbose = ctx.obj["verbose"]
+    debug = ctx.obj["debug"]
+    base_url = ctx.obj["base_url"]
+
+    # Login
+    if verbose:
+        click.echo("Logging in...")
+
+    params = {
+        "debug": debug
+    }
+
+    user = StudentLogin(base_url, **params)
+    user.login(emoji_code)
+
+    # Get user data
+    if verbose:
+        click.echo("Getting user data...")
+
+    user_data = user.me()
+
+    if verbose:
+        print(user_data)
+
+if __name__ == "__main__":
+   cli()
